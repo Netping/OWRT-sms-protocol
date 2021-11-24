1aa368917cb82da1c2fe8cae66594e63440912a2
Leonid Khlebushchev <lowpold1@gmail.com>
2021-11-24 21:19:26 +0300

    Epic.V0.S1 Task:31800369 Minor fixes for caption and config file

--- etc/commands/cmd_sendsms.py
+++ etc/commands/cmd_sendsms.py
@@ -9,6 +9,6 @@ from smsproto import SMSProto
 @click.option('-p', '--phone', required=True, help='Set phone number for send SMS')
 @click.option('-t', '--text', required=True, help='SMS text')
 def main(phone, text):
-    """Send email with text, subject, signature to destination address"""
+    """Send SMS with text to phone number"""
     ret = SMSProto.send(phone, text, False)
     sys.exit(ret)
